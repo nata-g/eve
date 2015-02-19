6066f2fbea7470285d99ff980096ba73880791a9
jamauro <jamauro@gmail.com>
2015-02-19 09:59:30 -0600

    Added data to the users collection

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,11 +4,13 @@ if (Users.find().count() === 0) {
     phone: '(231) 215-7062',
     email: 'lkohley8@gmail.com'
   });
+
   Users.insert({
     name: 'John Mauro',
     phone: '(210) 273-6251',
     email: 'jamauro@gmail.com'
   });
+
   Users.insert({
     name: 'Bob Marley',
     phone: '(512) 643-5498',
