2c1a43de80b261e0e1e3b1cb8dcaf191228f9e1c
jamauro <jamauro@gmail.com>
2015-02-27 08:57:12 -0600

    Basic error reporting

--- server/subscribe.js
+++ server/subscribe.js
@@ -2,14 +2,19 @@ var Stripe = StripeAPI('sk_test_4SRU3kmVuOF2xrvQAWiYTNMv');
 
 Meteor.methods({
   subscribe: function(token){
-    Stripe.customers.create({
-      source: token.id, // obtained with Stripe.js
-      plan: "eve",
-      email: token.email
-    }, function(err, customer) {
-      // asynchronously called
-    });
-    // how do i make this so it's only created on success?
-    Meteor.users.update(Meteor.userId(), {$set: {'profile.isSubscribed': true}});
+    // get a sync version of our API async func
+    var stripeCustomersCreateSync = Meteor.wrapAsync(Stripe.customers.create, Stripe.customers);
+    // call the sync version of our API func with the parameters from the method call
+    try {
+      var customer = stripeCustomersCreateSync({
+        source: token.id, // obtained with Stripe Checkout
+        plan: "eve",
+        email: token.email
+      });
+      Meteor.users.update(Meteor.userId(), {$set: {'profile.isSubscribed': true, stripe_id: customer.id}});
+    }
+    catch (err) {
+      console.log("error", err);
+    }
   }
 });
